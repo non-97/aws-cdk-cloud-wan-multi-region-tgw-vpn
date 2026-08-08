138ecd9f313cc3035d00c283b500cd67ab4f91df
non-97 <yamamoto.ryota@classmethod.jp>
2026-08-08 22:57:22 +0900

    Initial commit

---
+++ test/aws-cdk-cloud-wan-multi-region-tgw-vpn.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkCloudWanMultiRegionTgwVpn from '../lib/aws-cdk-cloud-wan-multi-region-tgw-vpn-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-cloud-wan-multi-region-tgw-vpn-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkCloudWanMultiRegionTgwVpn.AwsCdkCloudWanMultiRegionTgwVpnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
